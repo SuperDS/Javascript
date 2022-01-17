5dc4a844e929b1a9e1482cc2a52b49823a129401
이동섭 <ds4702@gmail.com>
2022-01-17 19:45:43 +0900

    #3 163p 2022_0117

---
+++ Day03_2022_0117/Array.js
@@ -0,0 +1,7 @@
+var empty =[];
+
+console.log(empty);
+
+var a =[2,[1, 2, 3, 4, 5] , 6];
+
+console.log(a);
---
+++ Day03_2022_0117/operator.js
@@ -0,0 +1,9 @@
+var a = 12
+var b = 15
+
+console.log(a === b);
+
+console.log(typeof a);
+
+var parity = (a % 2 === 0)? "짝수" : "홀수";
+console.log(parity);
