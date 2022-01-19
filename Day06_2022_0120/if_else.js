2b7ae117ac35cd70bb7cc957e2172c0293eafc95
이동섭 <ds4702@gmail.com>
2022-01-19 17:25:41 +0900

    65 620p 2022_0120

---
+++ Day06_2022_0120/if_else.js
@@ -0,0 +1,16 @@
+console.log("hi");
+
+var a = 1;
+console.log(a);
+
+if(a === 1)
+	a++;
+
+	console.log(a);
+
+
+var x = 12;
+if( x >= 0)
+	console.log("Positive or Zero");
+else
+	console.log("Negative");
